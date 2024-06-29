5281f688a3c98da8377deb9d3ebccf0f449ef68e
codeit-ninja <richard@codeit.ninja>
2024-06-29 14:04:30 +0200

    Adds radio

---
+++ src/lib/form/radio/index.svelte.ts
@@ -0,0 +1,12 @@
+import type { HTMLInputAttributes } from "svelte/elements";
+import Radio from "./Radio.svelte";
+
+export type RadioProps = {
+    label: string;
+    description?: string;
+    size?: 'sm' | 'md' | 'lg';
+} & Omit<HTMLInputAttributes, 'size'>;
+
+export {
+    Radio
+}
