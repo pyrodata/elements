fd863002b7c69f6274e13c762c4c547f2bcba5c1
codeit-ninja <richard@codeit.ninja>
2024-06-29 14:04:23 +0200

    Adds checkbox

---
+++ src/lib/form/checkbox/Checkbox.svelte
@@ -0,0 +1,60 @@
+<script lang="ts">
+    import type { CheckboxProps } from "./index.svelte.js";
+    import { c, uid } from "$lib/utils.js";
+    import { Check } from "lucide-svelte";
+
+    let {
+        id = uid(),
+        size = 'md',
+        label,
+        description,
+        checked = $bindable(false),
+        value = $bindable(),
+        ...props
+    }: CheckboxProps = $props();
+</script>
+<div class="flex gap-3">
+    <input
+        {id}
+        bind:checked
+        type="checkbox"
+        class={c(
+            'hidden',
+            'appearance-none',
+            'peer'
+        )}
+        {...props}
+    />
+    <span
+        class={c(
+            'relative',
+            'block',
+            'rounded-lg',
+            'border border-gray-500',
+            'peer-checked:bg-primary-600 peer-checked:text-white peer-checked:border-primary-600',
+            (size === 'sm' ? 'w-4 h-4 top-1.5' : ''),
+            (size === 'md' ? 'w-5 h-5 top-0.5' : ''),
+            (size === 'lg' ? 'w-6 h-6 top-0.5' : '')
+        )}
+    >
+        {#if checked}
+            <Check 
+                class={c(
+                    'absolute',
+                    'top-1/2 left-1/2',
+                    '-translate-x-1/2 -translate-y-1/2',
+                )}
+                strokeWidth="3"
+                size={
+                    (size === 'sm') ? 12 : 
+                    (size === 'md') ? 14 : 
+                    (size === 'lg') ? 18 : ''
+                }
+            />
+        {/if}
+    </span>
+    <label for={id} class="flex flex-col">
+        <span class="font-semibold">{label}</span>
+        <span class="text-gray-600">{@html description}</span>
+    </label>
+</div>
